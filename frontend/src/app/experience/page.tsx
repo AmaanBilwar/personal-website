fb87a4447a8a5e12f8acf8f2b08e3fb266391da1
Amaan Bilwar <bilwarad@mail.uc.edu>
2025-04-05 18:34:29 -0400

    all routes + basic page

---
+++ frontend/src/app/experience/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>xp</div>
+  )
+}
+
+export default page
---
+++ frontend/src/app/work/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>what im working on</div>
+  )
+}
+
+export default page
