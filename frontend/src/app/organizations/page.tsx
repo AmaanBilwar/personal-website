7848534be3751e6c86cfba790570af7671499322
Amaan Bilwar <bilwarad@mail.uc.edu>
2025-04-07 03:35:01 -0400

    org page

--- frontend/src/app/organizations/page.tsx
+++ frontend/src/app/organizations/page.tsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>orgs</div>
+    <main className="flex justify-center items-center min-h-screen">
+      <div className="text-center">
+        <h1>
+        bearcat motorsports
+        </h1>
+        <h1>
+          ieee
+        </h1>
+        <h1>
+          uc sase? 
+        </h1>
+        <h1>
+          uc ai club?
+        </h1>
+        <h1>
+          uc tech innovators
+        </h1>
+      </div>
+    </main>
   )
 }
 
