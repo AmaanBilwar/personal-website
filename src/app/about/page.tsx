0e068bd3328221bbde3ca83e11c64c3e1e083087
Amaan Bilwar <bilwarad@mail.uc.edu>
2025-10-15 08:06:52 -0400

    add:about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <main className=" text-white flex flex-col items-center justify-center h-screen">
+      <div>
+        <h1>about me</h1>
+      </div>
+    </main>
+  );
+};
+
+export default page;
