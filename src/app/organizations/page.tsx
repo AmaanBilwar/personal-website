65468032c67d55bd1fb9e6843f5ca87d4fc0a266
Amaan Bilwar <bilwarad@mail.uc.edu>
2025-10-15 08:07:04 -0400

    add: orgs page

---
+++ src/app/organizations/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <main className=" text-white flex flex-col items-center justify-center h-screen">
+      <div>
+        <h1>organizations</h1>
+      </div>
+    </main>
+  );
+};
+
+export default page;
